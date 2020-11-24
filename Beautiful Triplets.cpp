feddcef49d3e14179dd45f75e65563438a33a06f
Ahanabaiub <54981138+Ahanabaiub@users.noreply.github.com>
2020-11-24 15:55:18 +0600

    Add files via upload

---
+++ Beautiful Triplets.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n,d,i,j,k,cnt=0;
+    cin>>n>>d;
+
+    int a[n];
+
+    for(i=0; i<n; i++)
+        cin>>a[i];
+
+    for(i=0; i<n; i++)
+    {
+        for(j=i+1; j<n; j++)
+        {
+            if(a[j]-a[i]==d)
+            {
+                for(k=j+1; k<n; k++)
+                {
+                    if(a[k]-a[j]==d)
+                    {
+                        cnt++;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    cout<<cnt<<"\n";
+
+    return 0;
+}
